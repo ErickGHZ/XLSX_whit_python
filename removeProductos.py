83e9b3599eab5d62e2521384b183511541979c98
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-03-06 14:57:01 +0000

    arregle el remove productos y tiendas

--- removeProductos.py
+++ removeProductos.py
@@ -5,14 +5,21 @@ wb = load_workbook('ventas.xlsx')
 productos = wb['Productos']
 
 productoEliminado = input("Ingresa el SKU del prodcuto que deseas eliminar ")
-# Eliminar el registro con índice 2
+encontrado = False
 for row in productos.iter_rows():
     for cell in row:
         if cell.value == productoEliminado:
             productos.delete_rows(cell.row)
+            print("Producto eliminado correctamente")
+            encontrado = True
+            break
+
+if encontrado is False:  #  else en pocas palabras
+    print("Producto no encontrado")
+        
 
 
 # Guardar los cambios en un nuevo archivo xlsx
 wb.save('ventas.xlsx')
 
-# Mostrar la tiendas actualizada
+ 
